fee8e367516263da42852f61117c01c58f96e70f
Kevindecastro <9830@holbertonstudents.com>
2025-01-16 13:40:52 +0100

    correction code Convert Indentation to Spaces

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,9 +3,9 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10 # Get the last digit of the absolute value
+last_digit = abs(number) % 10
 if number < 0:
-    last_digit = -last_digit # If the number is negative, make the last digit negative
+    last_digit = -last_digit
 
 print(f"Last digit of {number} is {last_digit}", end=" ")
 
