39b6539b02487147e3484a73df9dcf548fe1e099
Kevindecastro <9830@holbertonstudents.com>
2025-01-16 15:01:15 +0100

    correction code

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,13 +5,14 @@ if __name__ == "__main__":
 
     a = 10
     b = 5
-    
+
     result = add(a, b)
     result1 = sub(a, b)
     result2 = mul(a, b)
     result3 = div(a, b)
-    
+
     print("{} + {} = {}".format(a, b, result))
     print("{} - {} = {}".format(a, b, result1))
     print("{} * {} = {}".format(a, b, result2))
     print("{} / {} = {}".format(a, b, result3))
+    
