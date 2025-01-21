21be99e6de8caca03268032b0e92206c563428ab
Kevindecastro <9830@holbertonstudents.com>
2025-01-21 15:51:44 +0100

    add 0-print_list_ interger a program that prints all integers in a list

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+
 def print_list_integer(my_list=[]):
+    """
+    Imprime tous les éléments d'une liste d'entiers.
+
+    Args:
+        my_list (list): Une liste d'entiers. Si aucune liste n'est fournie,
+        une liste vide est utilisée par défaut.
+    """
+    # Parcourt chaque élément de la liste
     for number in my_list:
+        # Imprime l'élément actuel formaté en tant qu'entier
         print("{:d}".format(number))
