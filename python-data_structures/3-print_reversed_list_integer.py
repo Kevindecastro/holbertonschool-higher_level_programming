149f55e481a33b08b8709efa257be02a474be2e0
Kevindecastro <9830@holbertonstudents.com>
2025-01-21 15:41:29 +0100

    add 3-print_reversed_list_integer.py a program that prints all integers in a list, in reverse order

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+
 def print_reversed_list_integer(my_list=[]):
+    """
+    Imprime tous les éléments d'une liste d'entiers dans l'ordre inverse.
+
+    Args:
+        my_list (list): Une liste d'entiers. Si aucune liste n'est fournie,
+        une liste vide est utilisée par défaut.
+    """
+    # Vérifie si la liste n'est pas vide
     if my_list:
+        # Parcourt la liste en ordre inverse
         for number in reversed(my_list):
+            # Imprime chaque entier formaté
             print("{:d}".format(number))
