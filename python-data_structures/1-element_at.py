ecb76bb92259c496f331865e06287313ba08d2bd
Kevindecastro <9830@holbertonstudents.com>
2025-01-21 15:48:28 +0100

    add 1-element_at.py a program that retrieves an element from a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
+    """
+    Récupère un élément d'une liste à un index spécifié.
+
+    Args:
+        my_list (list): La liste dont on veut récupérer un élément.
+        idx (int): L'index de l'élément à récupérer.
+
+    Returns:
+        L'élément à l'index spécifié si l'index est valide.
+        Retourne None si l'index est invalide.
+    """
+    # Vérifie si l'index est en dehors des limites de la liste
     if idx < 0 or idx >= len(my_list):
+        # Retourne None si l'index est invalide
         return None
+
+    # Retourne l'élément à l'index spécifié
     return my_list[idx]
