80c075180d24c11ed08aafadbde329d76eecf029
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 16:56:43 +0100

    add 5-raise_exception.py a program that raises an exception of type

---
+++ python-exceptions/5-raise_exception.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def raise_exception():
+    """
+    Lève une exception de type TypeError.
+
+    Cette fonction ne prend aucun argument et lève toujours une exception
+    de type TypeError lorsqu'elle est appelée.
+    """
+    raise TypeError  # Lève l'exception TypeError
