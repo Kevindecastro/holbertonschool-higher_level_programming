032c33012ad6ac3350098c9106dffa726c893e39
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 14:01:52 +0100

    correction made to validate pycodestyle

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,24 @@
 #!/usr/bin/python3
-def multiply_list_map(my_list=[], number=0):
-    # Utiliser map pour appliquer une fonction lambda qui multiplie chaque
-    # élément de la liste par 'number'
-    return list(map(lambda x: x * number, my_list))  # map applique la
-    # fonction à chaque élément de la liste, et list convertit le résultat
-    # en liste
+
+def multiply_list_map(my_list=None, number=0):
+    """
+    Multiplie chaque élément de la liste par 'number' et retourne une nouvelle
+    liste.
+
+    Args:
+        my_list (list, optional): Liste des éléments à multiplier. Par défaut,
+            c'est une liste vide.
+        number (int, float): Le facteur de multiplication.
+
+    Returns:
+        list: Une nouvelle liste où chaque élément est multiplié par 'number'.
+    """
+
+    # Si aucune liste n'est fournie,
+    # on initialise 'my_list' comme une liste vide.
+    if my_list is None:
+        my_list = []
+
+    # Utilisation d'une compréhension de liste pour multiplier chaque élément
+    # par 'number'. Cela crée une nouvelle liste avec les résultats.
+    return [x * number for x in my_list]
