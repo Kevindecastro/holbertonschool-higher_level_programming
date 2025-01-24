979f22534fe2fae69e520de4a7f4e06fd9904cb8
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 13:14:09 +0100

    add 7-update_dictionary.py a program that adds or updates a key in a dictionary with a given value

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+def update_dictionary(a_dictionary, key, value):
+    # Ajouter ou mettre à jour la paire clé-valeur dans le dictionnaire
+
+    a_dictionary[key] = value  # Attribuer la valeur à la clé spécifiée (si
+    # la clé existe, elle sera mise à jour)
+
+    # Retourner le dictionnaire mis à jour
+    return a_dictionary  # Retourne le dictionnaire après modification
