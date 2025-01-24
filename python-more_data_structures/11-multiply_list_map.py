1de6f4be529e1c2337c609b829888cebce349a5f
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 14:12:13 +0100

    correction made to validate max 3 lines

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# Utiliser map pour appliquer une fonction lambda qui multiplie chaque
-# élément de la liste par 'number'
 def multiply_list_map(my_list=[], number=0):
     return list(map(lambda x: x * number, my_list))
-# map applique la fonction à chaque élément de la liste,
-# et list convertit le résultat en liste
