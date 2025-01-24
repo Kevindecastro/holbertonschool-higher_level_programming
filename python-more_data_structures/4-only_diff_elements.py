597a808015063eaca27001041b5d62ab242f60ae
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 13:05:30 +0100

    add 4-only_diff_elements.py a program that returns a set of the elements present in only one of the given sets

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,5 @@
+def only_diff_elements(set_1, set_2):
+    # Utiliser l'opérateur ^ pour calculer la différence symétrique entre
+    # les deux ensembles
+    return set_1 ^ set_2  # Retourne un nouvel ensemble contenant les éléments
+    # qui sont dans set_1 ou set_2, mais pas dans les deux
