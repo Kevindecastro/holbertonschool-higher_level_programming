7b5643269ff6dd8a6bd578c6dc7a2f8fedf59e8c
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 13:08:21 +0100

    add 5-number_keys.py a program that returns the number of keys in a dictionary

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,5 @@
+def number_keys(a_dictionary):
+    # Utiliser la fonction len(),
+    # pour obtenir le nombre d'éléments (clés) dans le dictionnaire
+    # Retourne le nombre total de clés dans le dictionnaire
+    return len(a_dictionary)
