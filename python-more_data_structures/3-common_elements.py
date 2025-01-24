c6f2cde3bf066dc9021fcd09d364349fb8725225
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 13:03:42 +0100

    add 3-common_elements.py a program that returns a set containing the elements common to two given sets

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,4 @@
+def common_elements(set_1, set_2):
+    # Utiliser l'opérateur & pour calculer l'intersection des deux ensembles
+    return set_1 & set_2  # Retourne un nouvel ensemble contenant les éléments
+    # communs à set_1 et set_2
