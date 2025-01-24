c7815e097eff9656e1d54a9644a8ae8cbf1049c7
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 13:16:24 +0100

    add 8-simple_delete.py a program that deletes a specific key in a dictionary

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,8 @@
+def simple_delete(a_dictionary, key=""):
+    # Vérifier si la clé existe dans le dictionnaire
+    if key in a_dictionary:  # Si la clé est présente dans le dictionnaire
+        del a_dictionary[key]  # Supprimer l'élément correspondant à la clé
+
+    # Retourner le dictionnaire après la suppression (ou sans modification
+    # si la clé n'existe pas)
+    return a_dictionary  # Retourne le dictionnaire, modifié ou non
