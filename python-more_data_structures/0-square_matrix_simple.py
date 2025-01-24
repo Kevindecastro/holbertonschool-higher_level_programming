88b534e3cb5732c4cd3a5781128d7b9942a79c4f
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 11:17:08 +0100

    add 0-square_matrix_simple.py a program that calculates the square of all elements of a matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+def square_matrix_simple(matrix=[]):
+    # Utilisation d'une compréhension de liste pour parcourir chaque ligne
+    # (row) de la matrice
+    return [
+        [x ** 2 for x in row]  # Pour chaque élément 'x' dans la ligne,
+        # calculer son carré (x^2)
+        for row in matrix  # Parcourir chaque ligne de la matrice et appliquer
+        # l'opération à chaque élément
+    ]
