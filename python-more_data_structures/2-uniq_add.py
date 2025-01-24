0ebbdd5eac8ff7a7a98b42c7cac1589f70483bab
Kevindecastro <9830@holbertonstudents.com>
2025-01-24 11:43:22 +0100

    add 2-uniq_add.py a program that writes a function that adds all the unique integers in a list

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,4 @@
+def uniq_add(my_list=[]):
+    # Convertir la liste en un ensemble pour éliminer les doublons
+    return sum(set(my_list))  # Calculer la somme des éléments uniques de
+    # l'ensemble
