77ef821c9ef35d0f4d49af7c4fe681dd507aa23f
Kevindecastro <9830@holbertonstudents.com>
2025-01-26 18:41:07 +0100

    correctif synthax

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -17,12 +17,9 @@ def say_my_name(first_name, last_name=""):
         TypeError: Si first_name ou last_name ne sont pas des chaînes
                    de caractères.
     """
-    if not type(first_name) is str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not type(last_name) is str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-# Si last_name est vide, ne pas ajouter d'espace supplémentaire
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name}")
+
+    print(f"My name is {first_name} {last_name}")
