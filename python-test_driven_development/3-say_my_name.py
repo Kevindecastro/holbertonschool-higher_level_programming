ef6b793452a8e98a3314c28e538580cbf3b8ef6b
Kevindecastro <9830@holbertonstudents.com>
2025-01-26 18:54:46 +0100

    correctif synthax

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -17,6 +17,7 @@ def say_my_name(first_name, last_name=""):
         TypeError: Si first_name ou last_name ne sont pas des chaînes
                    de caractères.
     """
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
