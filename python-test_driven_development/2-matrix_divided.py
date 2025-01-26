10e70563e757f296a7d4cfd0ec80c6c6d4edc981
Kevindecastro <9830@holbertonstudents.com>
2025-01-26 17:55:30 +0100

    correctif pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-Divise tous les éléments d'une matrice par un diviseur donné,
-et retourne une nouvelle matrice avec les résultats arrondis à 2 décimales.
+Module 2-matrix_divided
+Contains a function that divides all elements of a matrix by a number.
 """
 
 
 def matrix_divided(matrix, div):
     """
-    Divise tous les éléments d'une matrice par un nombre donné.
+    Divise tous les éléments d'une matrice par un nombre donné,
+    et retourne une nouvelle matrice avec les résultats arrondis à 2 décimales.
 
     Args:
         matrix (list of list of int/float): La matrice à diviser.
@@ -24,21 +25,21 @@ def matrix_divided(matrix, div):
         list of list of float: Une nouvelle matrice avec les résultats
                                de la division.
     """
+    invalid_mat = "matrix must be a matrix (list of lists) of integers/floats"
 
-    # Vérifier que matrix est une liste non vide et une liste de listes
-    if (not isinstance(matrix, list) or len(matrix) == 0 or
-            not all(isinstance(row, list) for row in matrix)):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+    # Vérifier que matrix est une liste non vide
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError(invalid_mat)
+
+    # Vérifier que matrix est une liste de listes et qu'aucune ligne n'est vide
+    if not all(isinstance(row, list) for row in matrix) or any(
+            len(row) == 0 for row in matrix):
+        raise TypeError(invalid_mat)
 
     # Vérifier que chaque élément est un entier ou un flottant
-    if not all(
-        isinstance(element, (int, float)) for row in matrix for element in row
-    ):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+    if not all(isinstance(element, (int, float))
+               for row in matrix for element in row):
+        raise TypeError(invalid_mat)
 
     # Vérifier que toutes les lignes ont la même taille
     row_length = len(matrix[0])
@@ -53,5 +54,5 @@ def matrix_divided(matrix, div):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    # Diviser et arrondir les résultats à 2 décimales
+    # Effectuer la division et arrondir les résultats à 2 décimales
     return [[round(element / div, 2) for element in row] for row in matrix]
