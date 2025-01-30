0dac9ef4b249313c5ab9e757b9c6c7a32470fd41
Kevindecastro <9830@holbertonstudents.com>
2025-01-30 09:56:28 +0100

    add 0-square.py a program that creates a Square class with no attributes or methods

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""" Créer une classe Square sans attributs ni méthodes """
+
+
+class Square:
+    """ Classe représentant un carré """
+    pass  # La classe est vide pour l'instant, elle ne fait rien
