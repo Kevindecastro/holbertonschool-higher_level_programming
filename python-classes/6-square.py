c1763b67d4b53b74b92d5358feb81f07d2225c53
Kevindecastro <9830@holbertonstudents.com>
2025-01-31 15:28:01 +0100

    syntax correction of code @posiition.setter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -34,7 +34,7 @@ class Square:
             not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value# Si validé, on définit la position
+        self.__position = value  # Si validé, on définit la position
 
     def area(self):
         """ Retourne l'aire du carré """
