4c8ea0b097f3d23e002305320a70e7fa61ddbd26
Kevindecastro <9830@holbertonstudents.com>
2025-01-31 12:05:41 +0100

    modification of code line 9 and 12

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,9 +6,9 @@ class Square:
     """ Classe représentant un carré avec une validation de size """
     def __init__(self, size=0):
         # Vérification que size est un entier
-        if type(size) != int:
+        if not type(size) is int:
             raise TypeError("size must be an integer")
         # Vérification que size est un entier positif ou égal à zéro
-        if size < 0:
+        if int(size) < 0:
             raise ValueError("size must be >= 0")
         self.__size = size  # Si validé, on initialise l'attribut __size
