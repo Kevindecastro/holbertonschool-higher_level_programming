c6a037fd3d65c29fa7a697265ad01fa2ccef7d77
Kevindecastro <9830@holbertonstudents.com>
2025-01-31 13:30:00 +0100

    syntax correction

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Définit une classe Rectangle avec des attributs width et height."""
+"""Définit une classe Rectangle avec des attributs width et height"""
 
 
 class Rectangle:
-    """Représente un rectangle."""
+    """Représente un rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initialise le rectangle avec une largeur et une hauteur."""
+        """Initialise le rectangle avec une largeur et une hauteur"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Getter pour width."""
+        """Getter pour width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter pour width."""
+        """Setter pour width"""
         if not type(value) is int:
             raise TypeError("width must be an integer")
         if int(value) < 0:
@@ -26,12 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Getter pour height."""
+        """Getter pour height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter pour height."""
+        """Setter pour height"""
         if not type(value) is int:
             raise TypeError("height must be an integer")
         if int(value) < 0:
