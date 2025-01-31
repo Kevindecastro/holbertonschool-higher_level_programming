1bfb2dbf2d75040695962e0c5a04f8c9f80b825f
Kevindecastro <9830@holbertonstudents.com>
2025-01-31 12:09:53 +0100

    modification of code line 9 and 12

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,10 +6,10 @@ class Square:
     """ Classe représentant un carré avec un calcul de l'aire """
     def __init__(self, size=0):
         # Vérification que size est un entier
-        if type(size) != int:
+        if not type(size) is int:
             raise TypeError("size must be an integer")
         # Vérification que size est positif ou égal à zéro
-        if size < 0:
+        if int(size) < 0:
             raise ValueError("size must be >= 0")
         self.__size = size  # Initialisation de l'attribut __size
 
