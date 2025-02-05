88c2259eb70b88fbaa1b1b7c5503d3566a76d9d7
Kevindecastro <9830@holbertonstudents.com>
2025-02-05 15:08:57 +0100

    added an empty class representing a base for geometric objects

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module contenant une classe vide BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    Classe vide représentant une base pour des objets géométriques
+    """
+    pass
