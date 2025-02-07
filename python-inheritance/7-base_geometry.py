ddd6c6549bf262ef9bb08515d75a5aea26a3bb23
Kevindecastro <9830@holbertonstudents.com>
2025-02-07 15:20:52 +0100

    correction pycodestyle

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -6,33 +6,41 @@ Module contenant la classe BaseGeometry
 
 class BaseGeometry:
     """
-    La classe BaseGeometry représente une géométrie de base.
+    La classe BaseGeometry représente une géométrie de base
 
-    Cette classe est utilisée comme une classe de base pour d'autres formes géométriques.
-    Elle définit des méthodes communes pour calculer l'aire (qui n'est pas implémentée ici)
-    et pour valider les entiers dans des contextes géométriques.
+    Cette classe est utilisée comme une classe de base pour
+    d'autres formes géométriques
+    Elle définit des méthodes communes pour calculer l'aire
+    (qui n'est pas implémentée ici)
+    et pour valider les entiers dans descontextes
+    géométriques
     """
 
     def area(self):
         """
-        Méthode qui soulève une exception pour indiquer que la méthode n'est pas implémentée.
+        Méthode qui soulève une exception pour indiquer que
+        la méthode n'est pas implémentée
 
-        Cette méthode devrait être implémentée dans les classes filles pour calculer l'aire
-        d'une forme géométrique spécifique (par exemple, un rectangle, un cercle, etc.).
+        Cette méthode devrait être implémentée dans les
+        classes filles pour calculer l'aire
+        d'une forme géométrique spécifique
+        (par exemple, un rectangle, un cercle, etc.)
         """
         raise Exception("area() is not implemented")
-    
+
     def integer_validator(self, name, value):
         """
-        Valide que la valeur donnée est un entier et qu'il est supérieur à zéro.
+        Valide que la valeur donnée est un entier et
+        qu'il est supérieur à zéro
 
         Args:
-            name (str): Le nom de la valeur à valider.
-            value (int): La valeur à valider.
+            name (str): Le nom de la valeur à valider
+            value (int): La valeur à valider
 
         Raises:
-            TypeError: Si value n'est pas un entier.
-            ValueError: Si value est inférieur ou égal à zéro.
+            TypeError: Si value n'est pas un entier
+            ValueError: Si value est inférieur ou
+            égal à zéro
         """
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
