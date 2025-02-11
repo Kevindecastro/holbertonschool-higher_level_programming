291f2e0dd977354f861b9c5b6fe8b55573831bc7
Kevindecastro <9830@holbertonstudents.com>
2025-02-11 13:58:44 +0100

    added a module that allows to load a list of items from a JSON file, add the items passed in the command line and save the updated list in the same JSON file. If the file does not exist, it is created

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import sys
+import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+"""
+Ce module permet de charger une liste d'éléments à partir d'un fichier JSON,
+d'ajouter les éléments passés en ligne de commande et de sauvegarder la liste
+mise à jour dans le même fichier JSON. Si le fichier n'existe pas, il est
+créé.
+"""
+
+
+def main():
+    """
+    La fonction principale de ce module. Elle charge la liste d'éléments à
+    partir d'un fichier JSON, ajoute les éléments fournis en arguments via la
+    ligne de commande à cette liste, et enregistre la liste mise à jour dans
+    le fichier JSON.
+
+    Elle gère également le cas où le fichier JSON n'existe pas en initialisant
+    une nouvelle liste vide.
+    """
+    try:
+        # Tentative de chargement des éléments existants
+        # à partir du fichier JSON
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        # Si le fichier n'existe pas, on initialise une liste vide
+        items = []
+
+    # On ajoute les arguments passés en ligne de commande à la liste
+    items.extend(sys.argv[1:])
+
+    # Sauvegarde de la liste mise à jour dans le fichier JSON
+    save_to_json_file(items, "add_item.json")
+
+
+# On exécute la fonction principale si le fichier est exécuté directement
+if __name__ == "__main__":
+    main()
