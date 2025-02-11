b65390e6b25f5d9fd4b0125abf7018a37d9c072e
Kevindecastro <9830@holbertonstudents.com>
2025-02-11 12:07:28 +0100

    added a function to read a file and display its contents

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module contenant une fonction pour lire un fichier et afficher son contenu.
+"""
+
+
+def read_file(filename=""):
+    """
+    Lit un fichier texte (UTF8) et affiche son contenu sur la sortie standard.
+
+    Args:
+        filename (str): Le nom du fichier à lire.
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
