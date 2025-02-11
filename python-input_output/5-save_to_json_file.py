20a7c5bb3c51e4a84b47d6259e86111aefacbc62
Kevindecastro <9830@holbertonstudents.com>
2025-02-11 13:09:23 +0100

    added a function to save a Python object to a JSON file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+
+"""
+Module contenant une fonction pour enregistrer
+un objet Python dans un fichier JSON
+"""
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Enregistre un objet Python dans un fichier sous format JSON
+
+    Args:
+        my_obj (object): L'objet Python à enregistrer
+        filename (str): Le nom du fichier où enregistrer l'objet
+    """
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
