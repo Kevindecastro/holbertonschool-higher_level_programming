3002e3ab64cfa10e027a4d5c1999e17e5bb6b55c
Kevindecastro <9830@holbertonstudents.com>
2025-02-11 12:24:06 +0100

    added a function to add text to a file

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module contenant une fonction pour ajouter du texte à un fichier.
+"""
+
+def append_write(filename="", text=""):
+    """
+    Ajoute une chaîne de caractères à la fin d'un fichier texte (UTF8)
+    et retourne le nombre de caractères ajoutés.
+
+    Args:
+        filename (str): Le nom du fichier.
+        text (str): Le texte à ajouter dans le fichier.
+
+    Returns:
+        int: Nombre de caractères ajoutés.
+    """
+    with open(filename, "a", encoding="utf-8") as file:
+        return file.write(text)
