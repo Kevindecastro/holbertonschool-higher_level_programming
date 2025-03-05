ebff6af1d4557018f6232cac58abee30cc29597d
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 13:26:40 +0100

    add a script that creates a SQL script to list user privileges

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Vérifier si l'utilisateur 'user_0d_1' existe déjà et le créer si ce n'est pas le cas
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Accorder tous les privilèges sur toutes les bases de données à 'user_0d_1'
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Appliquer les changements
+FLUSH PRIVILEGES;
