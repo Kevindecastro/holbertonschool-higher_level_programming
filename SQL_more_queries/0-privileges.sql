ce3628c13d1179f041cc2b95c6d4bdb6fb4b2afe
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 12:56:59 +0100

    add a script that lists all privileges of MySQL users user_0d_1 and user_0d_2

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Répertorie tous les privilèges des utilisateurs MySQL user_0d_1 et user_0d_2
+
+-- Vérifier les privilèges pour l'utilisateur 'user_0d_1' sur 'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Vérifier les privilèges pour l'utilisateur 'user_0d_2' sur 'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
