e0c962aa1a91887785ac9ba02248861f143cc169
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 12:20:29 +0100

    add a script that selects the score and name columns from the second_table table and sorts the results by descending score (highest to lowest)

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Sélectionne les colonnes score et name de la table second_table
+-- et trient les résultats par score décroissant (du plus grand au plus petit)
+SELECT score, name FROM second_table ORDER BY score DESC;
