2fc2870f680d4d6a52ef81681247db22f34148e5
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 16:01:52 +0100

    add a script that lists the genres associated with the shows

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,5 @@
+-- Liste les genres associés aux émissions
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
