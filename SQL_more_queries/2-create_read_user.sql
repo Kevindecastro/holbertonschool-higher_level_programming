41a986db600f0d96a9a88c424dbf18a572dcf053
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 13:40:41 +0100

    fix erreur

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,9 @@
--- Crée une base de données `hbtn_0d_2` et un utilisateur `user_0d_2` avec le privilège `SELECT`.
+-- Crée une base de données `hbtn_0d_2` et un utilisateur `user_0d_2` avec le privilège `SELECT`
 
 -- Créer une base de données si elle n'existe pas
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- Créer un utilisateur s'il n'existe pas
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- Accorder uniquement le privilège Select
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 -- Appliquer les changements
