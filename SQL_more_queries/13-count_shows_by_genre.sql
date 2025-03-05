4f230443ef638a834b07231f2816996ca26b869d
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 16:27:33 +0100

    fix erreur

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,7 +9,7 @@ JOIN tv_show_genres tsg ON g.id = tsg.genre_id
 JOIN tv_show s ON tsg.tv_show_id = s.id
 
 -- Regroupe les résultats par genre pour compter combien d'émissions sont associées à chaque genre
-GROUP BY g.id
+GROUP BY g.name
 
 -- Exclut les genres qui n'ont pas d'émissions associées
 HAVING COUNT(s.id) > 0
