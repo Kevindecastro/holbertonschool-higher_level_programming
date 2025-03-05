9aa0349938d6317050e7e4422718903b56b731af
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 16:31:06 +0100

    fix erreur

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,14 +5,8 @@ FROM tv_genres g
 -- Jointure avec la table de liaison `tv_show_genres` pour associer chaque genre à ses émissions
 JOIN tv_show_genres tsg ON g.id = tsg.genre_id
 
--- Jointure avec la table `tv_show` pour s'assurer qu'on compte bien les émissions existantes
-JOIN tv_show s ON tsg.tv_show_id = s.id
-
 -- Regroupe les résultats par genre pour compter combien d'émissions sont associées à chaque genre
 GROUP BY g.id
-
--- Exclut les genres qui n'ont pas d'émissions associées
-HAVING COUNT(g.id) > 0
 
 -- Trie les résultats par ordre décroissant du nombre d'émissions par genre
 ORDER BY number_of_shows DESC;
