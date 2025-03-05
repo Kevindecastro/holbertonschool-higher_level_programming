93e68bb3d4987a722f15437daadaba99829f39a8
Kevindecastro <9830@holbertonstudents.com>
2025-03-05 14:04:58 +0100

    fix erreur

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,5 @@
 -- Crée une table `id_not_null` où la colonne `id` ne peut pas être NULL
 CREATE TABLE IF NOT EXISTS id_not_null (
     id INT DEFAULT 1,
-    name VARCHAR(256),
+    name VARCHAR(256)
 );
