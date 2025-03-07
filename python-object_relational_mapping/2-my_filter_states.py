e3a66d562b32330e8b6f51cb54c30eee075b381e
Kevindecastro <9830@holbertonstudents.com>
2025-03-07 16:07:41 +0100

    add a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Lists all states where 'name' matches the user input from the database."""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Connexion à la base de données MySQL avec les arguments fournis
+    db = MySQLdb.connect(
+        host="localhost",  # Hôte de la base de données
+        user=sys.argv[1],  # Nom d'utilisateur MySQL
+        passwd=sys.argv[2],  # Mot de passe MySQL
+        db=sys.argv[3],  # Nom de la base de données
+        port=3306  # Port MySQL par défaut
+    )
+
+    # Création du curseur pour exécuter la requête
+    cur = db.cursor()
+
+    # Construction et exécution de la requête SQL
+    query = (
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY '{}' "
+        "ORDER BY states.id ASC"
+    ).format(sys.argv[4])  # Utilisation de format() pour insérer l'entrée utilisateur
+
+    cur.execute(query)
+
+    # Affichage des résultats
+    for row in cur.fetchall():
+        print(row)
+
+    # Fermeture du curseur et de la connexion à la base de données
+    cur.close()
+    db.close()
