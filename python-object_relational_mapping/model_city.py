091ac54ea5c10a8aa25591fb9dd5b93730809573
Kevindecastro <9830@holbertonstudents.com>
2025-03-07 18:41:09 +0100

    fix error

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -3,17 +3,12 @@
 Defines the City class and creates the Base instance.
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
-    """
-    Represents a city in the database.
-    """
+    """City class"""
     __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
