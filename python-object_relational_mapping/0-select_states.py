b7b24d67cfb7beb91f1f45a55d64d6fbca15ed5c
Kevindecastro <9830@holbertonstudents.com>
2025-03-07 15:01:36 +0100

    fix erreur

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,25 +7,25 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-	# Connexion à la base de données
-	db = MySQLdb.connect(
-		host="localhost",
-		user=sys.argv[1],
-		passwd=sys.argv[2],
-		db=sys.argv[3],
-		port=3306
-		)
+    # Connexion à la base de données
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
 
-	# Création d'un curseur
-	cur = db.cursor()
+    # Création d'un curseur
+    cur = db.cursor()
 
-	# Exécution de la requête SQL
-	cur.execute("SELECT * FROM states ORDER BY id ASC")
+    # Exécution de la requête SQL
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-	# Récupération et affichage des résultats
-	for row in cur.fetchall():
-		print(row)
+    # Récupération et affichage des résultats
+    for row in cur.fetchall():
+        print(row)
 
-	# Fermeture du curseur et de la connexion
-	cur.close()
-	db.close()
+    # Fermeture du curseur et de la connexion
+    cur.close()
+    db.close()
