0782f8723c8f7a88948262f219dc673657209456
Kevindecastro <9830@holbertonstudents.com>
2025-04-03 11:16:29 +0200

    add the code

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,9 @@
+// 3-script.js
+document.querySelector('#toggle_header').addEventListener('click', function() {
+	const header = document.querySelector('header');
+	if (header.classList.contains('red')) {
+	  header.classList.replace('red', 'green');
+	} else {
+	  header.classList.replace('green', 'red');
+	}
+  });
---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,7 @@
+// 8-script.js
+fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+  .then(response => response.json())
+  .then(data => {
+    document.querySelector('#hello').textContent = data.hello;
+  })
+  .catch(error => console.error('Error fetching hello:', error));
